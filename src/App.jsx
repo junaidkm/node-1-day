6a34c985b4a6b2f00346bb40f63f39b33b33e844
Junaid <junaidntd@gmail.com>
2026-06-16 16:27:05 +0530

    update project

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 function App() {
   return (
-    <div>ebdsbbsdgdgfsbbdv v v v v v v v v v v v v v v v v v v v v v vv v v v v v v v v v v v v v v v v v v v v v v v v v v v v v v v v v v v v v v v v v v v v v  v v v v v v v v bbbestbs</div>
+    <div>ebdsbbsdgdgfsbdv v v v v v v v v v v v v v v v v v v v v v vv v v v v v v v v v v v v v v v v v v v v v v v v v v v v v v v v v v v v v v v v v v v v v  v v v v v v v v bbbestbs</div>
   )
 }
 
